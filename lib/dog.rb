327b439df8fb480d1fd39f015a053a04aac63084
Sara Harvey <saraharveynyc@gmail.com>
2018-08-30 17:52:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,19 +5,13 @@ class Dog
   end
 
   def bark
-    @bark = bark
+    @bark = puts "woof!"
   end
 
   def name=(new_name)
     @name = new_name
   end
-
-  def bark=(new_bark)
-    @bark = new_bark
-  end
 end
-
-@bark = puts "woof"
 
 fido = Dog.new
 fido.name = "Fido"
